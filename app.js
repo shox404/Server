969977946d151285f90fcd42ca9b0413aa03ad4d
shox404 <shxxx404@gmail.com>
2024-09-11 17:14:36 +0500

    Set up

--- app.js
+++ app.js
@@ -9,14 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./middleware/error.js"));
 app.use(compression());
 
-app.get("/", (_, res) => {
-  return res.send("<a href='/route'>Click to redirect to /route</a>");
-});
-
-app.use("/route", require("./routes/route"));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
